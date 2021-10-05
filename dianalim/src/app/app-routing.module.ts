104801b65bdfffd412384c4e251b7615fe8e4ddc
diana-lim <51867385+diana-lim@users.noreply.github.com>
2021-10-05 00:16:49 -0700

    Configured routing for portfolio and projects pages

--- dianalim/src/app/app-routing.module.ts
+++ dianalim/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioProjectsComponent } from './portfolio-projects/portfolio-projects.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'portfolio-projects',
+    component: PortfolioProjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- dianalim/src/app/app.module.ts
+++ dianalim/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,19 +10,38 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { ProjectsComponent } from './projects/projects.component';
+import { PortfolioProjectsComponent } from './portfolio-projects/portfolio-projects.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProjectsComponent,
+    PortfolioProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BsDropdownModule,
     TooltipModule,
-    ModalModule
+    ModalModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent
+      },
+      {
+        path: 'portfolio-projects',
+        component: PortfolioProjectsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
